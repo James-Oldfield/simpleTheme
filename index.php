0f9e0ebccbacd1bf0bb48aedd9eef2f5b9a5aa85
James Oldfield <hi@james-oldfield.co>
2014-10-20 23:37:03 +0100

    Removed annoying margin-top on first p tag in index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 
 <div class="main home">           
-	<p>Hi.</p>
+	<p class="first">Hi.</p>
 	<p>My name's James.</p>
 	<p>I'm a freelance web developer and designer based in London, UK.</p>
 	<p>Feel free to check out some of my <a href="/work">work</a>, read my <a href="/blog">blog</a> or <a href="mailto:hi@james-oldfield.co">hire me</a>.</p>
