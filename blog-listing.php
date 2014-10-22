6e4e8045dd94f0321ebd12c4d0e479cdc08e31cf
James Oldfield <hi@james-oldfield.co>
2014-10-22 17:10:52 +0100

    changed blog error 2

--- blog-listing.php
+++ blog-listing.php
@@ -7,16 +7,13 @@ Template Name: blog template
 
 <div class="main blog">
 
-<button id="readMore">READ MORE</button>
-
 <?php
 query_posts('cat=16');
 while (have_posts()) : the_post(); ?>
         <div class="blogPost">
 
-
             <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><h1><?php the_title(); ?></h1></a>
-            <p id="blogContent"><?php the_content(); ?></p>
+            <p><?php the_content(); ?></p>
             
         </div>    
 <?php endwhile; ?>
