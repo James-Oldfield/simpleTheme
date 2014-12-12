62a43cca68a27f0d38eb6041c159354b9b0eaaf9
James Oldfield <hi@james-oldfield.co>
2014-12-12 17:50:12 +0000

    removed title on homepage

--- index.php
+++ index.php
@@ -13,7 +13,6 @@
 		while (have_posts()) : the_post(); ?>
 		        <div class="post-thumb">
 		            <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('post-thumb'); ?></a>
-		            <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><h1><?php the_title(); ?></h1></a>
 		        </div>    
 		<?php endwhile; ?>
 		</div>
