5487cf4455b022ff43b97d3e7c05a50b8c83ec25
James Oldfield <hi@james-oldfield.co>
2014-12-12 17:47:38 +0000

    updated homepage

--- index.php
+++ index.php
@@ -7,6 +7,16 @@
 		<div class="about">
 			<p>I'm a freelance web developer and designer based in London, UK.<br>Feel free to check out some of my <a href='/work'><span>work</span></a>, read my <a href='/blog'><span>blog</span></a> or <a href='mailto:hi@james-oldfield.co'><span>hire me</span></a>.</p>
 		</div>
+		<div class="homeGrid">
+		<?php
+		query_posts('cat=15');
+		while (have_posts()) : the_post(); ?>
+		        <div class="post-thumb">
+		            <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('post-thumb'); ?></a>
+		            <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><h1><?php the_title(); ?></h1></a>
+		        </div>    
+		<?php endwhile; ?>
+		</div>
 	</div>
 </div>
 
